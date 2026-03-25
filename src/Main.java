03b6b33f88263fcb76f3b869fc7e1840a6589cdc
sreehithreddy0202 <absreehithr@gmail.com>
2026-03-25 09:22:16 +0530

    refactor: rename class to Main for UC2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrainConsistManagementApp {
+public class Main {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
